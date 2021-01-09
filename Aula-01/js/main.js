837d0bda49390e0d200aa08f2ec386a059b1a259
Danilson Sanches <dsanches06@outlook.com>
2021-01-09 14:55:00 +0000

    Introdução a javascript, com variavies, operações aritmeticas e operações com string

---
+++ Aula-01/js/main.js
@@ -0,0 +1,18 @@
+var nome = "Danilson Sanches";
+var n1 = 29;
+var n2 = 10;
+var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + n1 + " anos");
+
+//operações com string
+console.log(nome + " tem " + n1 + " anos");
+console.log(frase.replace("Japão", "Cabo Verde"));//usar string.replace para substituir palavras
+console.log(frase.toLowerCase());//passar para letras minusculas
+console.log(frase.toUpperCase());//passar para letras maiusculas
+
+//operações aritmeticas
+console.log(n1 + n2);
+console.log(n1 - n2);
+console.log(n1 * n2);
+console.log(n1 / n2);
+console.log(n1 % n2);
